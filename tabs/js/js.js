b2ddec9dafc67157ccb7e263fc8be85c14a5d2ac
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2020-06-26 15:15:49 +0300

    work at tabs in tab

--- tabs/js/js.js
+++ tabs/js/js.js
@@ -41,9 +41,22 @@ const getTabPart = (tabPart, classPart, i = 0) => {
 for (let t = 0; t < tabs.length; t++) {
     let sumWidth = 0;
     const events = ['load', 'resize', 'click'];
+
+
+
+
+
     const titles = tabs[t].getElementsByClassName(cList.tabTitle)[0].children;
-    
+
+    // const titles = [];
+    // for(const title of tabs[t].children){         
+    //     if (title.classList.contains('titles')) {for(const child of title.children){ titles.push(child)}};
+    // }    
+
     console.log("titles", titles);
+
+
+
 
     const contents = [];
     for(const content of tabs[t].children){         
@@ -54,25 +67,59 @@ for (let t = 0; t < tabs.length; t++) {
     
     getTabPart(titles, cList.activeTitle);
     getTabPart(contents, cList.activeContent);
+
     const contentBorderRadius = getParams(contents[0], cList.borderRadiusTR); 
-    for (let i = 0; i < titles.length; i++) {    
+    console.log("contentBorderRadius", contentBorderRadius);
+    for (let i = 0; i < titles.length; i++) {   
+
+        console.log("titles[i]", titles[i]);
+        console.log("titles[i]", titles[i].clientWidth);
         sumWidth += titles[i].clientWidth + getParams(titles[i], cList.borderLeft) + getParams(titles[i], cList.borderRight);
+        console.log("sumWidth", sumWidth);
+
         for(const event of events){
             ((event === 'click') ? titles[i] : window).addEventListener(event, function() { 
+                const contentActive = tabs[t].getElementsByClassName(cList.activeContent)[0];
                 if (event === 'click') {
                     getTabPart(this.parentElement.children, cList.activeTitle, i);
                     getTabPart(contents, cList.activeContent, i);
-                }
 
-                // console.log("iiii", tabs[t]);
+                    // const contentschild = [];
+                    // for(const child of this.parentElement.parentElement.children){         
+                    //     child.classList.contains('content') ? contentschild.push(child) : null;
+                    // }
+                    // console.log("ooooooo", contentschild[i].getElementsByClassName(cList.tabWrap));
+                    // console.log("ooooooolll", contentschild[i].getElementsByClassName(cList.tabWrap).length);
+                    // if (contentschild[i].getElementsByClassName(cList.tabWrap).length > 0) {
+
+                    // }  
+                    
+                    
+                    console.log("-------", this.parentElement.parentElement.getElementsByClassName(cList.activeContent)[0]);
+                }      
                 
-                const contentActive = tabs[t].getElementsByClassName(cList.activeContent)[0];
+                console.log("yyyyyyyyy", tabs[t].children);
+                console.log("yyyyyyyyyyyyy", tabs[t].getElementsByClassName(cList.activeContent)[0]);
+
+           
+
+                
+                // const contentActive = tabs[t].getElementsByClassName(cList.activeContent)[0];
                 const contentWidth = contentActive.clientWidth + getParams(contentActive, cList.borderLeft) + getParams(contentActive, cList.borderRight);
                 sumWidth = (config[t] == 1) ? contentWidth : sumWidth;
+
+                console.log("sumWidth", sumWidth);
+                console.log("contentActive", contentActive);
+                console.log("contentWidth", contentWidth);
+
+
+                
                 titles[i].classList.remove(cList.borderRadius);
                 titles[i].offsetTop === 0 ? titles[i].classList.add(cList.borderRadius) : null;
+
                 titles[i].style.flexGrow = (config[t] == 1) ? config[t] 
                     : ((config[t] == 0) && (sumWidth >= contentWidth)) ? 1 : 0;
+
                 contentActive.style.borderTopRightRadius = (sumWidth >= contentWidth) ? '0px' 
                     : ((sumWidth < contentWidth) && ((sumWidth + contentBorderRadius) > contentWidth)) ? `${contentWidth - sumWidth}px` 
                     : `${contentBorderRadius}px`;
