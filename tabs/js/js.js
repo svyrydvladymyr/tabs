714a14c551996435f34280b57280a8a41991a9ba
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2020-06-30 18:04:13 +0300

    work at tabs in tab

--- tabs/js/js.js
+++ tabs/js/js.js
@@ -12,14 +12,6 @@ const cList = {
 const tabs = document.getElementsByClassName(cList.tabWrap);
 console.log("tabs", tabs);
 
-// const config = { tabTitileParams: `${(function() {
-//     for(const tab of tabs){       
-//         const param = tab.getAttribute('param');
-//         return (!isNaN(param) && param !== undefined && (param == 1 || param == 0)) ? param : 0; 
-//     }})()}`};
-        
-// console.log("config", config);
-
 const config = [];
 (function() {
     for(const tab of tabs){       
@@ -29,7 +21,6 @@ const config = [];
 })();
     
 console.log("config", config);
-
 
 const getParams = (el, param) => parseInt(getComputedStyle(el).getPropertyValue(param));
 const getTabPart = (tabPart, classPart, i = 0) => {
@@ -42,10 +33,6 @@ for (let t = 0; t < tabs.length; t++) {
     let sumWidth = 0;
     const events = ['load', 'resize', 'click'];
 
-
-
-
-
     const titles = tabs[t].getElementsByClassName(cList.tabTitle)[0].children;
 
     // const titles = [];
@@ -54,9 +41,6 @@ for (let t = 0; t < tabs.length; t++) {
     // }    
 
     console.log("titles", titles);
-
-
-
 
     const contents = [];
     for(const content of tabs[t].children){         
@@ -72,57 +56,58 @@ for (let t = 0; t < tabs.length; t++) {
     console.log("contentBorderRadius", contentBorderRadius);
     for (let i = 0; i < titles.length; i++) {   
 
+        console.log("titles", titles);
         console.log("titles[i]", titles[i]);
-        console.log("titles[i]", titles[i].clientWidth);
+        console.log("titles[ic", titles[i].clientWidth);
         sumWidth += titles[i].clientWidth + getParams(titles[i], cList.borderLeft) + getParams(titles[i], cList.borderRight);
         console.log("sumWidth", sumWidth);
 
         for(const event of events){
             ((event === 'click') ? titles[i] : window).addEventListener(event, function() { 
-                const contentActive = tabs[t].getElementsByClassName(cList.activeContent)[0];
+
                 if (event === 'click') {
+
+                    console.clear();                    
+
                     getTabPart(this.parentElement.children, cList.activeTitle, i);
                     getTabPart(contents, cList.activeContent, i);
+                }                   
 
-                    // const contentschild = [];
-                    // for(const child of this.parentElement.parentElement.children){         
-                    //     child.classList.contains('content') ? contentschild.push(child) : null;
-                    // }
-                    // console.log("ooooooo", contentschild[i].getElementsByClassName(cList.tabWrap));
-                    // console.log("ooooooolll", contentschild[i].getElementsByClassName(cList.tabWrap).length);
-                    // if (contentschild[i].getElementsByClassName(cList.tabWrap).length > 0) {
-
-                    // }  
-                    
-                    
-                    console.log("-------", this.parentElement.parentElement.getElementsByClassName(cList.activeContent)[0]);
-                }      
-                
                 console.log("yyyyyyyyy", tabs[t].children);
                 console.log("yyyyyyyyyyyyy", tabs[t].getElementsByClassName(cList.activeContent)[0]);
 
-           
-
+                const contentActive = [];
+                $(document).ready(function(){ for (const i of $(tabs[t]).children()) { i.classList.contains('content_active') ? contentActive.push(i) : null }});
                 
-                // const contentActive = tabs[t].getElementsByClassName(cList.activeContent)[0];
-                const contentWidth = contentActive.clientWidth + getParams(contentActive, cList.borderLeft) + getParams(contentActive, cList.borderRight);
-                sumWidth = (config[t] == 1) ? contentWidth : sumWidth;
+                setTimeout(() => {         
 
-                console.log("sumWidth", sumWidth);
-                console.log("contentActive", contentActive);
-                console.log("contentWidth", contentWidth);
+                    console.log("000000000", tabs[t].getElementsByClassName(cList.activeContent)[0]);
+                    
+                    // const contentActive = contentAa[0];
+                    console.log("000000000", contentActive[0]);
+                    
+                    // const contentActive[0] = tabs[t].getElementsByClassName(cList.activeContent)[0];
+                    const contentWidth = contentActive[0].clientWidth + getParams(contentActive[0], cList.borderLeft) + getParams(contentActive[0], cList.borderRight);
+                    
+                    console.log("config[t]", config[t]);
 
+                    sumWidth = (config[t] == 1) ? contentWidth : sumWidth;
 
-                
-                titles[i].classList.remove(cList.borderRadius);
-                titles[i].offsetTop === 0 ? titles[i].classList.add(cList.borderRadius) : null;
+                    console.log("sumWidth", sumWidth);
+                    console.log("contentActive[0]", contentActive[0]);
+                    console.log("contentWidth", contentWidth);
 
-                titles[i].style.flexGrow = (config[t] == 1) ? config[t] 
-                    : ((config[t] == 0) && (sumWidth >= contentWidth)) ? 1 : 0;
+                    titles[i].classList.remove(cList.borderRadius);
+                    titles[i].offsetTop === 0 ? titles[i].classList.add(cList.borderRadius) : null;
 
-                contentActive.style.borderTopRightRadius = (sumWidth >= contentWidth) ? '0px' 
-                    : ((sumWidth < contentWidth) && ((sumWidth + contentBorderRadius) > contentWidth)) ? `${contentWidth - sumWidth}px` 
-                    : `${contentBorderRadius}px`;
+                    titles[i].style.flexGrow = (config[t] == 1) ? config[t] 
+                        : ((config[t] == 0) && (sumWidth >= contentWidth)) ? 1 : 0;
+
+                    contentActive[0].style.borderTopRightRadius = (sumWidth >= contentWidth) ? '0px' 
+                        : ((sumWidth < contentWidth) && ((sumWidth + contentBorderRadius) > contentWidth)) ? `${contentWidth - sumWidth}px` 
+                        : `${contentBorderRadius}px`;
+
+                }, 1);
             });
         };
     };
